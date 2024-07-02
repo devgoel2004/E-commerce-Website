64aa1433de17091a1a6b3a7cb62b48b0da25c9fb
Dev <devgoel2004@gmail.com>
2024-07-02 23:40:35 +0530

    Added ProtectedRoute and Create Product Page

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -2,8 +2,26 @@ const Product = require("../models/productModel");
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const ApiFeatures = require("../utils/apiFeatures");
+const cloudinary = require("cloudinary");
 //create a product -- Admin
 exports.createProduct = catchAsyncErrors(async (req, res, next) => {
+  let images = [];
+  if (typeof req.body.images === "string") {
+    images.push(req.body.images);
+  } else {
+    images = req.body.images;
+  }
+  const imagesLink = [];
+  for (let i = 0; i < images.length; i++) {
+    const result = await cloudinary.v2.uploader.upload(images[i], {
+      folder: "products",
+    });
+    imagesLink.push({
+      public_id: result.public_id,
+      url: result.secure_url,
+    });
+  }
+  req.body.images = imagesLink;
   req.body.user = req.user.id;
   const product = await Product.create(req.body);
   res.status(201).json({
